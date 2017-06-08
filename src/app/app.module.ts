faddea2da86319bdf7cbf293d85757228598d318
Mathews Antony <mathews.antony@allianz.com>
2017-06-08 17:12:55 +0100

    Added angualt material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MdIconModule, MdListModule, MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdCardModule, MdRadioModule, MdSliderModule, MdProgressBarModule
+  , MdProgressSpinnerModule, MdToolbarModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +17,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule, MdToolbarModule,
+    MdButtonModule, MdCheckboxModule,
+    MdIconModule, MdListModule, MdCardModule, MdRadioModule, MdSliderModule,
+    MdProgressBarModule, MdProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
